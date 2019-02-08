4149f33cbc76bae637ca3b8cf8b40a7907b8c9a2
BKC\edgar <Edgar.vanOostrum@bkcmedia.nl>
2019-02-08 16:30:25 +0100

    videocapture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MediaCapture } from '@ionic-native/media-capture/ngx';
+import { Media } from '@ionic-native/media/ngx';
+import { File } from '@ionic-native/file/ngx';
+
+import { IonicStorageModule } from '@ionic/storage';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot() ],
   providers: [
     StatusBar,
     SplashScreen,
+    { provide: MediaCapture, useClass: MediaCapture },
+    { provide: Media, useClass: Media },
+    { provide: File, useClass: File },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MediaCapture } from "@ionic-native/media-capture/ngx";
+import { Media } from "@ionic-native/media/ngx";
+import { File } from "@ionic-native/file/ngx";
+import { CaptureVideoOptions, MediaFile } from "@ionic-native/media-capture";
+import { Storage } from '@ionic/storage';
 
+const MEDIA_FILES_KEY = 'mediafiles';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+    @ViewChild('myvideo') myVideo: any;
+    mediaFiles = [];
+
+    constructor(private mediaCapture: MediaCapture, private storage: Storage, private media: Media, private file: File) {
+
+    }
+
+    captureAudio() {
+        this.mediaCapture.captureAudio().then(res => {
+            this.storeMediaFiles(res);
+        })
+    }
+
+    captureVideo() {
+        let options: CaptureVideoOptions = {
+            limit: 1,
+            duration: 30
+        }
+        this.mediaCapture.captureVideo(options).then((res: MediaFile[]) => {
+            this.storeMediaFiles(res);
+        })
+    }
+
+    storeMediaFiles(files) {
+        this.storage.get(MEDIA_FILES_KEY).then(res=> {
+            if(res){
+                let arr = JSON.parse(res);
+                arr = arr.concat(files);
+                this.storage.set(MEDIA_FILES_KEY, JSON.stringify(arr));
+            }else{
+                this.storage.set(MEDIA_FILES_KEY, JSON.stringify(files));
+            }
+            this.mediaFiles = this.mediaFiles.concat(files);
+        })
+    }
+}
+
+
+
+
